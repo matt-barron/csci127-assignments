9e93b3cbabeed9bdba56bd2ecaab8408c31f70a4
Matthew Barron <mattbarron@Matthews-MacBook-Air.local>
2018-09-30 20:10:07 -0400

    hw_03

---
+++ hw_03/collatz.py
@@ -0,0 +1,14 @@
+#Kaitlyn Zhen Matthew Barron
+def F(n): 
+ while n != 1:
+        if n % 2 == 0:
+            n = n/2
+            print(n)
+        elif n % 2 != 0:
+            n = (3*n)+1
+            print(n)
+        else:
+            print(n)
+        
+    
+    
