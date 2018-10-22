7dd6c4a7ffbdd5ea2bed6344a843cc70f2e12367
Matthew Barron <mattbarron@Matthews-MacBook-Air.local>
2018-10-22 09:21:53 -0400

    exam 1

---
+++ exam_01/compression.py
@@ -0,0 +1,22 @@
+def compress_word(w):
+    """
+    W is a parameter representing a word that is a string. This
+    function should remove the letters " A, E, I, O, U" from a word.
+    """
+    newword = " "
+    vowels= ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    for i in range(len(w)):
+        if w[i] not in vowels :
+            newword= w[i] + newword
+                       
+    return newword[::-1]
+    
+
+print(compress_word("hello"))
+print(compress_word("apple"))
+def sentence(line):
+
+    return compress_word(line)
+print(sentence(" who is there"))
+print(sentence("i like to eat apple pie"))
+
