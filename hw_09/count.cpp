53dbfe47792d02d0766c542072a86461f236b7f6
Matthew Barron <mattbarron@Matthews-MacBook-Air.local>
2018-12-03 03:42:25 -0500

    hw9

---
+++ hw_09/count.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number, i = 1+i, factorial = 1 ;
+  while ( i <= 19)
+    {
+       std::cout << i << std::endl;
+       int number, i = 1+i, factorial = 1;
+    };
+};
+ 
